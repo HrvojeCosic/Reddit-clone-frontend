5073a6806519596bc45b3991eed6fdacdf9c7f78
Hrvoje Cosic <hcacc100@gmail.com>
2020-09-27 09:26:19 +0200

    Added seperate reset.css file, moved header styles to a seperate file

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <div id="app">
-    <Header />
-    <MainPage />
-    <!-- <router-link to="/">Home</router-link> |
+	<div id="app">
+		<Header />
+		<MainPage />
+		<!-- <router-link to="/">Home</router-link> |
 			<router-link to="/about">About</router-link> -->
-  </div>
-  <!-- <router-view /> -->
+	</div>
+	<!-- <router-view /> -->
 </template>
 
 <script>
-import Header from "./components/Header/Header.vue";
-import MainPage from "./components/MainPage.vue";
+import Header from './components/Header/Header.vue';
+import MainPage from './components/MainPage.vue';
 export default {
-  components: { Header, MainPage },
+	components: { Header, MainPage },
 };
 </script>
 
 <style scoped>
-@import "./components/Header/Header.css";
+@import './reset.css';
 #app {
-  width: 100vw;
-  height: 100vh;
-  background-color: #dae0e6;
+	width: 100vw;
+	height: 100vh;
+	background-color: #dae0e6;
 }
 </style>
