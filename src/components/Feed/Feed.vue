e0ebb64dee8ee7e61889df5ee570e6552ae23579
Hrvoje Cosic <hcacc100@gmail.com>
2020-09-27 15:04:47 +0200

    Feature only unique communities on feed, removed x-axis scroll on html element

--- src/components/Feed/Feed.vue
+++ src/components/Feed/Feed.vue
@@ -6,9 +6,9 @@
 		<div class="featured-communities-container">
 			<h1 class="featured-communities-title">FeaturedCommunities</h1>
 			<FeaturedCommunities
-				v-for="post in postsData"
-				v-bind:key="post.id"
-				v-bind:community="post.community"
+				v-for="community in uniqueCommunities"
+				:key="community"
+				:community="community"
 			/>
 		</div>
 	</div>
@@ -21,6 +21,17 @@ export default {
 	name: 'Feed',
 	props: ['postsData'],
 	components: { Post, FeaturedCommunities },
+	computed: {
+		uniqueCommunities() {
+			const postsData = this.$props.postsData;
+			let uniqueComms = [];
+			postsData.forEach(post => {
+				uniqueComms.push(post.community);
+			});
+			uniqueComms = new Set(uniqueComms);
+			return uniqueComms;
+		},
+	},
 };
 </script>
 
