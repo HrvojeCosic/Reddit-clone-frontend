19fe1c08a479c290457a4414c3af58e73c855073
Hrvoje Cosic <hcacc100@gmail.com>
2020-10-06 10:26:04 +0200

    Display number of upvotes

--- src/components/Post/Post.vue
+++ src/components/Post/Post.vue
@@ -2,12 +2,12 @@
 	<div id="post">
 		<div class="voting">
 			<div class="upvote" @click="post.upvotes++" />
-			<div class="number-of-votes">{{ post.upvotes - post.downvotes }}</div>
+			<div class="number-of-votes">{{ post.upvotes }}</div>
 			<div class="downvote" @click="post.upvotes--" />
 		</div>
 		<div class="upper-part">
 			<a class="community">r/{{ post.subreddit }}</a>
-			<a class="posted-by">u/{{ post.postedBy }}</a>
+			<a class="posted-by">u/{{ post.author[0].username }}</a>
 			<a class="date-posted">{{ post.timestamp }}</a>
 		</div>
 		<div class="post-title">{{ post.title }}</div>
